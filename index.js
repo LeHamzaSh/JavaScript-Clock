9e89f6ffdb0e8fa539643b2c3ab7c72a749eff81
LeHamzaSh <hrsheikh05@gmail.com>
2019-09-01 22:13:04 +0100

    finished clock

--- index.js
+++ index.js
@@ -9,5 +9,13 @@ function setClock(){
     const seconds = dateToday.getSeconds() / 60
     const minutes = (seconds + dateToday.getMinutes()) / 60
     const hours = (minutes + dateToday.getHours()) / 12
+    setRotation(secondHand, seconds)
+    setRotation(minuteHand, minutes)
+    setRotation(hourHand, hours)
 }
 
+function setRotation(element, rotationRatio){
+    element.style.setProperty('--rotate', rotationRatio * 360)
+}
+
+setClock()
